207d8f451ddca8b55d6a48c1d40c365d41c002cf
yongjun choi <lambert52851@gmail.com>
2022-12-14 20:22:20 +0900

    added vivado

---
+++ hardware/src/bd/system/ip/system_util_ds_buf_0_0/system_util_ds_buf_0_0_stub.vhdl
@@ -0,0 +1,31 @@
+-- Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2017.4 (lin64) Build 2086221 Fri Dec 15 20:54:30 MST 2017
+-- Date        : Fri Mar 23 14:29:47 2018
+-- Host        : ubuntu running 64-bit Ubuntu 16.04.3 LTS
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/digilent/work/git/Zybo-Z7-10-base-linux/src/bd/system/ip/system_util_ds_buf_0_0/system_util_ds_buf_0_0_stub.vhdl
+-- Design      : system_util_ds_buf_0_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z010clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity system_util_ds_buf_0_0 is
+  Port ( 
+    BUFG_I : in STD_LOGIC_VECTOR ( 0 to 0 );
+    BUFG_O : out STD_LOGIC_VECTOR ( 0 to 0 )
+  );
+
+end system_util_ds_buf_0_0;
+
+architecture stub of system_util_ds_buf_0_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "BUFG_I[0:0],BUFG_O[0:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "util_ds_buf,Vivado 2017.4";
+begin
+end;
